de8e6387e48420cdd1744024ce31b71a0a215ea3
Piyush <egrpiyush@gmail.comm>
2020-08-26 22:22:10 +1000

    Added GetMonthlyPremiumQuery.

---
+++ PremiumCalculationService.Application/Queries/GetMonthlyPremium/GetMonthlyPremiumQuery.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PremiumCalculationService.Application.Queries.GetMonthlyPremium
+{
+    public class GetMonthlyPremiumQuery : IRequest<MonthlyPremium>
+    {
+        public decimal CoverAmount { get; set; }
+        public int OccupationId { get; set; }
+        public decimal Age { get; set; }
+
+        public class Handler : IRequestHandler<GetMonthlyPremiumQuery, MonthlyPremium>
+        {
+            public Handler()
+            {
+
+            }
+            public Task<MonthlyPremium> Handle(GetMonthlyPremiumQuery request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ PremiumCalculationService.Application/Queries/GetMonthlyPremium/MonthlyPremium.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PremiumCalculationService.Application.Queries.GetMonthlyPremium
+{
+    public class MonthlyPremium
+    {
+        public decimal Amount { get; set; }
+    }
+}
