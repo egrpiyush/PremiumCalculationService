7aac71cd17e9676bfc05fb45b6ace989191ed26c
Piyush <egrpiyush@gmail.comm>
2020-08-27 00:02:10 +1000

    Added Startup and Autofac wild card search.

---
+++ PremiumCalculationService.Application/AutofacModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using PremiumCalculationService.Common;
+
+namespace PremiumCalculationService.Application
+{
+    public class AutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterMatchingTypeAsImplementedInterfaces(typeof(AutofacModule).Assembly,
+                new[] { ".+Command$", ".+Query$", ".+Service$" });
+        }
+    }
+}
---
+++ PremiumCalculationService.Functions/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using MediatR;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using PremiumCalculationService.Application;
+using PremiumCalculationService.Functions;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace PremiumCalculationService.Functions
+{
+    public class Startup : FunctionsStartup
+    {
+        public IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public Startup()
+        {
+
+        }
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var applicationAssembly = typeof(AutofacModule).GetTypeInfo().Assembly;
+            builder.Services
+                .AddMediatR(applicationAssembly);
+
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterModule<AutofacModule>();
+            var container = containerBuilder.Build();
+            var serviceProvider = new AutofacServiceProvider(container);
+        }
+    }
+}
