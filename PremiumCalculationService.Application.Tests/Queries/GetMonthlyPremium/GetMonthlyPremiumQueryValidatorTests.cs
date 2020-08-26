7f473fa4cc9d71faf8614c7558ca15a8b25d76c5
Piyush <egrpiyush@gmail.comm>
2020-08-26 23:49:07 +1000

    Added GetMonthlyPremiumQueryValidator and unit tests.

---
+++ PremiumCalculationService.Application.Tests/Queries/GetMonthlyPremium/GetMonthlyPremiumQueryValidatorTests.cs
@@ -0,0 +1,56 @@
+﻿using AutoFixture;
+using FluentValidation.TestHelper;
+using PremiumCalculationService.Application.Queries.GetMonthlyPremium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PremiumCalculationService.Application.Tests.Queries.GetMonthlyPremium
+{
+    public class GetMonthlyPremiumQueryValidatorTests
+    {
+        private readonly GetMonthlyPremiumQueryValidator _rulesValidator;
+        private static readonly Fixture Fixture = new Fixture();
+        public GetMonthlyPremiumQueryValidatorTests()
+        {
+            _rulesValidator = new GetMonthlyPremiumQueryValidator();
+        }
+
+        [Fact]
+        public void WhenCoverAmountIsLessThanZero_ShouldHaveValidationError()
+        {
+            var sut = Fixture.Build<GetMonthlyPremiumQuery>()
+                    .With(p => p.CoverAmount, -1)
+                    .Create();
+            _rulesValidator.ShouldHaveValidationErrorFor(p => p.CoverAmount, sut);
+        }
+
+        [Fact]
+        public void WhenCoverAmountIsZero_ShouldHaveValidationError()
+        {
+            var sut = Fixture.Build<GetMonthlyPremiumQuery>()
+                    .Without(p => p.CoverAmount)
+                    .Create();
+            _rulesValidator.ShouldHaveValidationErrorFor(p => p.CoverAmount, sut);
+        }
+
+        [Fact]
+        public void WhenOccupationIdIsInvalid_ShouldHaveValidationError()
+        {
+            var sut = Fixture.Build<GetMonthlyPremiumQuery>()
+                    .Without(p => p.OccupationId)
+                    .Create();
+            _rulesValidator.ShouldHaveValidationErrorFor(p => p.OccupationId, sut);
+        }
+
+        [Fact]
+        public void WhenAgeIsInvalid_ShouldHaveValidationError()
+        {
+            var sut = Fixture.Build<GetMonthlyPremiumQuery>()
+                    .Without(p => p.Age)
+                    .Create();
+            _rulesValidator.ShouldHaveValidationErrorFor(p => p.Age, sut);
+        }
+    }
+}
---
+++ PremiumCalculationService.Application/Queries/GetMonthlyPremium/GetMonthlyPremiumQueryValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PremiumCalculationService.Application.Queries.GetMonthlyPremium
+{
+    public class GetMonthlyPremiumQueryValidator : AbstractValidator<GetMonthlyPremiumQuery>
+    {
+        public GetMonthlyPremiumQueryValidator()
+        {
+            RuleFor(p => p.CoverAmount)
+                .GreaterThan(0);
+            RuleFor(p => p.OccupationId)
+                .GreaterThan(0);
+            RuleFor(p => p.Age)
+                .GreaterThan(0);
+        }
+    }
+}
