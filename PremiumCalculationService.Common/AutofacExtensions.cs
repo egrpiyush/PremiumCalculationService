189105891347e064c22e473af8720056b57c4722
Piyush <egrpiyush@gmail.comm>
2020-08-26 23:52:26 +1000

    Added AutofacExtensions.

---
+++ PremiumCalculationService.Common/AutofacExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PremiumCalculationService.Common
+{
+    public static class AutofacExtensions
+    {
+        public static void RegisterMatchingTypeAsImplementedInterfaces(this ContainerBuilder builder, Assembly containingAssembly, 
+            params string[] classNameMatchers)
+        {
+            builder
+                .RegisterAssemblyTypes(containingAssembly)
+                .Where(p => classNameMatchers.Any(m => Regex.IsMatch(p.Name, m)))
+                .AsImplementedInterfaces();
+        }
+    }
+}
