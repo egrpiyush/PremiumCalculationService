0c26e9c106886c6614348e3d00cd1c8ec4c86a9e
Piyush <egrpiyush@gmail.comm>
2020-08-26 13:24:35 +1000

    Added entities.

---
+++ PremiumCalculationService.Domain/Entities/Occupation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PremiumCalculationService.Domain.Entities
+{
+    public class Occupation
+    {
+        public string Name { get; set; }
+        public Rating Rating { get; set; }
+    }
+}
